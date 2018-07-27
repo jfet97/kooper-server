94abf93933f3c32aeb571ad02609bb084ca9617f
Andrea Simone Costa <jfet97@users.noreply.github.com>
2018-07-27 13:06:06 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,28 +7,7 @@ const PORT = process.env.PORT || 3000;
 const PORT_SOCKET = 5000;
 
 app.use(helmet());
-// app.use(cors({ origin: 'http://localhost:5000', credentials: true }));
 app.use(cors());
-
-/*
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
-    // intercept OPTIONS method
-    if ('OPTIONS' === req.method) {
-      res.send(200);
-    }
-    else {
-      next();
-    }
-};
-
-app.use(allowCrossDomain);
-*/
-
 app.use(express.json()); // middleware per il parsing del json nelle richieste POST
 app.use(express.static('build'));
 
@@ -41,10 +20,11 @@ const server_socket = app.listen(PORT_SOCKET, () => {
 
 
 const io = socket(server_socket);
-io.origins('*:*')
 
 io.on('connection', (socket) => {
-   
+    io.sockets.emit('sfondo', {
+        colore: "giallo"
+    })
 });
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
