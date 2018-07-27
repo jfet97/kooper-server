29aca6ed4b3575276d094ce91d72960d19a11736
Andrea Simone Costa <jfet97@users.noreply.github.com>
2018-07-27 12:27:49 +0200

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ app.use(helmet());
 // app.use(cors());
 
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://93.35.247.99/');
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
