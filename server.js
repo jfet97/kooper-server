c25d77efb4b227de1d5d02cfda2cd61981bd7c22
Andrea Simone Costa <jfet97@users.noreply.github.com>
2018-07-27 13:08:44 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const socket = require('socket.io');
 const app = express();
 const PORT = process.env.PORT || 3000;
-const PORT_SOCKET = 5000;
+const PORT_SOCKET = process.env.PORT || 5000;
 
 app.use(helmet());
 app.use(cors());
